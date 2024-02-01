298da4d05425ba19a1e1a1be558eda76e9e47fd4
seulgi chu <seulgichu14@gmail.com>
2024-02-01 22:16:45 +0900

    📦️ [Chore] 보일러플레이트 초안 작성
    express, mongoose, eslint, husky, prettier, lint-staged, dotenv, cors, nodemon 설치 및 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["airbnb", "airbnb/hooks", "plugin:prettier/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}", "bin/www"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["prettier"],
+  rules: {
+    semi: "warn",
+    "no-unused-vars": "warn",
+    "no-param-reassign": 0,
+  },
+};
---
+++ src/loaders/index.js
@@ -0,0 +1,3 @@
+const appLoader = async (app) => {};
+
+module.exports = appLoader;
