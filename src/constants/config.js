e077961009a0d846ce1dff4485f3ef19e631f548
kyeongjun-ko <gyeongjungo@Gyeongjunui-MacBookAir.local>
2024-02-03 02:28:16 +0900

    🔨 [Feat] Rebase 후 버전 요청시 개발용 환경 변수 추가
    rebase 후 환경설정 파일 config.js 에서 개발용 토큰, 파일키 추가

--- src/constants/config.js
+++ src/constants/config.js
@@ -4,6 +4,8 @@ const CONFIG = {
   PORT: process.env.PORT,
   MONGODB_ENDPOINT: process.env.MONGODB_ENDPOINT,
   CLIENT_URL: process.env.CLIENT_URL,
+  DEV_TOKEN: process.env.DEV_FIGMA_SECRET_TOKEN,
+  DEV_FILE_KEY: process.env.DEV_FIGMA_TEST_FILE_KEY,
 };
 
 module.exports = CONFIG;
