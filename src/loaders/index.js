582e915a2d845f5539cfb86e7a0d0e2cd23ba9e6
seulgi chu <seulgichu14@gmail.com>
2024-02-02 20:20:07 +0900

    📦️ [Chore] appLoader 연결 및 express 설정 추가
    appLoader 내 express, mongoose, router Loader 연결 및 express 설정 추가

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,3 +1,11 @@
-const appLoader = async (app) => {};
+const mongooseLoader = require("./mongoose");
+const expressLoader = require("./express");
+const routerLoader = require("./routers");
+
+const appLoader = async (app) => {
+  await mongooseLoader();
+  await expressLoader(app);
+  await routerLoader(app);
+};
 
 module.exports = appLoader;
