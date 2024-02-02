4e5e3c0a40d257fe30be8ac57a18a4afd1fcd6c0
kyeongjun-ko <gyeongjungo@Gyeongjunui-MacBookAir.local>
2024-02-03 02:36:51 +0900

    ✨ [Feat] Rebase 후 버전API 요청시 응답 및 에러처리
    rebase 후 기존 브랜치 작업 내용 적용

--- src/routers/projects.js
+++ src/routers/projects.js
@@ -2,8 +2,91 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", function (req, res, next) {
+router.get("/", function (req, res) {
   res.status(200).send({ sucess: true });
+});
+
+router.get("/:projectId/versions", async (req, res) => {
+  const { projectId } = req.params;
+  const accessToken = req.headers.authorization;
+
+  if (accessToken) {
+    try {
+      const getVersions = await fetch(
+        `https://api.figma.com/v1/files/${projectId}/versions`,
+        {
+          method: "GET",
+          headers: {
+            "X-FIGMA-TOKEN": accessToken,
+          },
+        },
+      );
+
+      const responseJson = await getVersions.json();
+      const { status, versions } = responseJson;
+
+      if (status === 400) {
+        return res.status(400).send({
+          result: "error",
+          error: {
+            message: "Bad Request",
+          },
+        });
+      }
+
+      if (status === 403) {
+        return res.status(403).send({
+          result: "error",
+          error: {
+            message: "인증되지 않은 사용자 접근입니다.",
+          },
+        });
+      }
+
+      if (status === 404) {
+        return res.status(404).send({
+          result: "error",
+          error: {
+            message: "존재하지 않는 피그마 파일입니다. 다시 입력해주세요",
+          },
+        });
+      }
+
+      if (status === 500) {
+        return res.status(500).send({
+          result: "error",
+          error: {
+            message: "프로젝트 용량이 너무 커 응답에 실패했습니다.",
+          },
+        });
+      }
+
+      if (versions.length < 2) {
+        return res.status(502).send({
+          result: "error",
+          error: {
+            message: "비교할 수 있는 버전이 없어서 서비스 이용이 불가합니다.",
+          },
+        });
+      }
+
+      return res.status(200).send(responseJson);
+    } catch (e) {
+      return res.status(500).send({
+        result: "error",
+        error: {
+          message: "Internal Server Error",
+        },
+      });
+    }
+  } else {
+    return res.status(403).send({
+      result: "error",
+      error: {
+        message: "인증되지 않은 사용자입니다.",
+      },
+    });
+  }
 });
 
 module.exports = router;
