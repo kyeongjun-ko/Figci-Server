a5e3ee00adb5b813804e815f0ac9cdd70e2b4b85
seulgi chu <seulgichu14@gmail.com>
2024-02-02 23:02:03 +0900

    🐛 [Fix] cors 설정 추가
    origin, credentials, methods, optionsSucessStatus 설정 추가

--- src/constants/config.js
+++ src/constants/config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const CONFIG = {
   PORT: process.env.PORT,
   MONGODB_ENDPOINT: process.env.MONGODB_ENDPOINT,
+  CLIENT_URL: process.env.CLIENT_URL,
 };
 
 module.exports = CONFIG;
--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const CONFIG = require("../constants/config");
 
 async function expressLoader(app) {
-  app.use(cors());
+  app.use(
+    cors({
+      origin: CONFIG.CLIENT_URL,
+      credentials: true,
+      methods: "GET, POST",
+      optionsSuccessStatus: 204,
+    }),
+  );
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
 }
