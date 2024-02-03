7f1ebfe1e939e0c2b3a2a1bb279d2c0c0ec34b9e
kyeongjun-ko <gyeongjungo@Gyeongjunui-MacBookAir.local>
2024-02-03 18:24:16 +0900

    🔥 [Remove] 사용하지 않는 라우터 파일 삭제
    서버 환경 테스트시 사용하는 라우터 파일 삭제

--- src/loaders/routers.js
+++ src/loaders/routers.js
@@ -1,8 +1,6 @@
 const projectRouter = require("../routers/projects");
-const indexRouter = require("../routers/index");
 
 async function routerLoader(app) {
-  app.use("/", indexRouter);
   app.use("/projects", projectRouter);
 }
 
--- src/routers/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-router.get("/", function (req, res, next) {
-  res.status(200).send({ sucess: true });
-});
-
-module.exports = router;
