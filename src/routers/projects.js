13e5d98e972b88af2886bda8c442c0ad2a5576ac
seulgi chu <seulgichu14@gmail.com>
2024-02-05 11:01:07 +0900

    ♻️ [Refactor] 불필요한 라우터 연결 제거
    불필요한 라우터 연결 로직 제거

--- src/routers/projects.js
+++ src/routers/projects.js
@@ -4,10 +4,6 @@ const projectController = require("../controllers/projectController");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).send({ success: true });
-});
-
 router.get("/:projectId/versions", projectController.getAllVersions);
 router.get("/:projectId/pages", projectController.getCommonPages);
 
