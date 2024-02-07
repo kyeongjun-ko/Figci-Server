577dc9873117f33cfbbbc02da02abd0c7f9157ce
tjd985 <tjd985@gmail.com>
2024-02-07 23:25:20 +0900

    ✨ [Feat] page diffing logic 구현
    page diffing 로직 구현

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -1,9 +1,10 @@
-/* eslint-disable consistent-return */
 const createHttpError = require("http-errors");
 const Document = require("../models/Document");
+const Result = require("../models/Result");
 
 const comparePages = require("../utils/comparePages");
 const saveFigmaDataAsNestedStructure = require("../utils/saveFigmaDataAsNestedStructure");
+const getDiffing = require("../utils/getDiffing");
 const CONSTANT = require("../constants/constants");
 const ERROR = require("../constants/error");
 
@@ -75,6 +76,7 @@ const getCommonPages = async (req, res, next) => {
 
   const createDocument = async (projectKey, versionId) => {
     const figmaData = await fetchFigmaData(projectKey, versionId);
+
     const document = await saveFigmaDataAsNestedStructure(
       figmaData.document,
       null,
@@ -114,4 +116,114 @@ const getCommonPages = async (req, res, next) => {
   }
 };
 
-module.exports = { getAllVersions, getCommonPages };
+const getStoredDiffingResult = async (
+  projectKey,
+  beforeVersionId,
+  afterVersionId,
+  pageId,
+) => {
+  const storedDiffingResult = await Result.findOne({
+    projectKey,
+    beforeVersionId,
+    afterVersionId,
+    pageId,
+  });
+
+  return storedDiffingResult;
+};
+
+const createDiffingResult = async (
+  projectKey,
+  beforeVersionId,
+  afterVersionId,
+  pageId,
+) => {
+  const getDocument = async (projectKey, versionId) => {
+    const document = await Document.findOne({
+      projectKey,
+      versionId,
+    });
+
+    return document;
+  };
+
+  const beforeDocument = await getDocument(projectKey, beforeVersionId);
+  const afterDocument = await getDocument(projectKey, afterVersionId);
+
+  const beforeFrameList = beforeDocument.pages.get(pageId).frames;
+  const afterFrameList = afterDocument.pages.get(pageId).frames;
+
+  const diffingResult = {
+    projectKey,
+    beforeVersionId,
+    afterVersionId,
+    pageId,
+    frames: new Set(),
+    differences: {},
+  };
+
+  await getDiffing(beforeFrameList, afterFrameList, diffingResult);
+
+  const diffingInstance = await Result.create(diffingResult);
+
+  return diffingInstance;
+};
+
+const getModifiedFrame = async (projectKey, versionId, pageId, frameId) => {
+  const modifiedDocument = await Document.findOne({
+    projectKey,
+    versionId,
+  });
+
+  const modifiedPage = modifiedDocument.pages.get(pageId);
+  const modifiedFrame = modifiedPage.frames.get(frameId);
+
+  return modifiedFrame;
+};
+
+const getDiffingResult = async (req, res, next) => {
+  const { projectId: projectKey, pageId } = req.params;
+  const beforeVersionId = req.query["before-version"];
+  const afterVersionId = req.query["after-version"];
+
+  try {
+    const diffingResult =
+      (await getStoredDiffingResult(
+        projectKey,
+        beforeVersionId,
+        afterVersionId,
+        pageId,
+      )) ||
+      (await createDiffingResult(
+        projectKey,
+        beforeVersionId,
+        afterVersionId,
+        pageId,
+      ));
+
+    const modifiedFrameIdList = diffingResult.frames;
+    diffingResult.frames = [];
+
+    for (const modifiedFrameId of modifiedFrameIdList) {
+      const modifiedFrame = await getModifiedFrame(
+        projectKey,
+        afterVersionId,
+        pageId,
+        modifiedFrameId,
+      );
+
+      diffingResult.frames.push(modifiedFrame);
+    }
+
+    res.status(200).json(diffingResult);
+  } catch (err) {
+    const customError = createHttpError(
+      ERROR.SERVER_ERROR.status,
+      ERROR.SERVER_ERROR.message,
+    );
+
+    next(customError);
+  }
+};
+
+module.exports = { getAllVersions, getCommonPages, getDiffingResult };
