aebce41162e193e6f9c19067dfed6b1c218c23e4
tjd985 <tjd985@gmail.com>
2024-02-07 23:26:51 +0900

    ✨ [Feat] diffing 컨트롤러 추가
    diffing 컨트롤러 추가

--- src/routers/projects.js
+++ src/routers/projects.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.get("/:projectId/versions", projectController.getAllVersions);
 router.get("/:projectId/pages", projectController.getCommonPages);
+router.get("/:projectId/pages/:pageId/", projectController.getDiffingResult);
 
 module.exports = router;
