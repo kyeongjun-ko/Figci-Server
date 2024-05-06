726f86135fdeaed1863db9d6bc6df9f5f362c9c0
kyeongjun-ko <rhrudwnszoq@naver.com>
2024-05-06 17:57:56 +0900

    ♻️ [Refactor] GridFs 데이터 처리 로직 에러핸들링을 위한 변수 추가

--- src/constants/error.js
+++ src/constants/error.js
@@ -19,6 +19,18 @@ const ERROR_PATTERNS = {
     status: 500,
     message: "Can't access Figma info; file too large.🥲",
   },
+  GRIDFS_ERROR: {
+    status: 500,
+    message: "GridFS error",
+  },
+  GRIDFS_UPLOAD_ERROR: {
+    status: 500,
+    message: "GridFS File upload failed",
+  },
+  DOCUMENT_NOT_FOUND: {
+    status: 404,
+    message: "Document not found",
+  },
 };
 
 module.exports = ERROR_PATTERNS;
