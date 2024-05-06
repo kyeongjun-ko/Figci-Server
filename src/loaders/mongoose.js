99711f61a42287f488889e336bfc74c3cfff1a6e
kyeongjun-ko <rhrudwnszoq@naver.com>
2024-05-06 17:51:01 +0900

    ✨ [Feat] 몽구스 loader 함수 GridFSBucket 버킷 설정 로직 추가

--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -1,17 +1,31 @@
 const mongoose = require("mongoose");
+const { GridFSBucket } = require("mongodb");
 
 const CONFIG = require("../constants/config");
+const CONSTANT = require("../constants/constants");
 
-async function mongooseLoader() {
+let bucket;
+
+const connectToMongoDB = async () => {
   try {
-    await mongoose.connect(CONFIG.MONGODB_ENDPOINT, {
-      DBName: "Figci",
-    });
-
-    console.log("connected to database");
+    await mongoose.connect(CONFIG.MONGODB_ENDPOINT, { dbName: CONFIG.DB_NAME });
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error(error);
+    console.error("MongoDB connection error:", error);
+    throw error;
   }
-}
+};
 
-module.exports = mongooseLoader;
+const initBucket = async () => {
+  const db = mongoose.connection.client.db();
+  bucket = new GridFSBucket(db, { bucketName: CONSTANT.BUCKET_NAME });
+};
+
+const mongooseLoader = async () => {
+  await connectToMongoDB();
+  await initBucket();
+};
+
+const getBucket = () => bucket;
+
+module.exports = { mongooseLoader, getBucket };
