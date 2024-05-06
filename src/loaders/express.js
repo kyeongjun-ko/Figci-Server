67be13b2b4ce9ebb6c0427c9418210550782e06b
kyeongjun-ko <rhrudwnszoq@naver.com>
2024-05-06 17:42:27 +0900

    ✏️ [Style] 함수 선언시 함수표현식으로 코드 컨벤션 통일

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
-async function expressLoader(app) {
+const expressLoader = async (app) => {
   app.set("view engine", "pug");
   app.set("views", `${__dirname}/../views`);
   app.use(express.static(path.join(__dirname, "../public")));
@@ -17,6 +17,6 @@ async function expressLoader(app) {
   );
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-}
+};
 
 module.exports = expressLoader;
--- src/loaders/routers.js
+++ src/loaders/routers.js
@@ -2,10 +2,10 @@ const projectRouter = require("../routers/projects");
 const oauthRouter = require("../routers/oauth");
 const userRouter = require("../routers/user");
 
-async function routerLoader(app) {
+const routerLoader = async (app) => {
   app.use("/projects", projectRouter);
   app.use("/oauth", oauthRouter);
   app.use("/user", userRouter);
-}
+};
 
 module.exports = routerLoader;
