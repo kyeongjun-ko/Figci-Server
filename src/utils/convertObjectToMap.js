e656b80c4dd1ad183ecf9a37e2bab668409fce71
kyeongjun-ko <rhrudwnszoq@naver.com>
2024-05-11 22:43:59 +0900

    ♻️ [Refactor] gridFS 파일 Map 변환함수 isOwnProperty 함수 적용

--- src/utils/convertObjectToMap.js
+++ src/utils/convertObjectToMap.js
@@ -1,26 +1,33 @@
+const isOwnProperty = require("./isOwnProperty");
+
 const convertObjectToMap = (gridFSObject) => {
   if (gridFSObject && typeof gridFSObject === "object") {
     if (Array.isArray(gridFSObject)) {
       return gridFSObject.map(convertObjectToMap);
     }
+
     const convertedObject = {};
 
     for (const key in gridFSObject) {
-      if (key === "pages" || key === "frames" || key === "nodes") {
-        const convertedMap = new Map();
+      if (isOwnProperty(gridFSObject, key)) {
+        if (key === "pages" || key === "frames" || key === "nodes") {
+          const convertedMap = new Map();
 
-        for (const [pageId, pageNodes] of Object.entries(gridFSObject[key])) {
-          convertedMap.set(pageId, convertObjectToMap(pageNodes));
-          convertedMap[pageId] = convertObjectToMap(pageNodes);
+          for (const [pageId, pageNodes] of Object.entries(gridFSObject[key])) {
+            convertedMap.set(pageId, convertObjectToMap(pageNodes));
+            convertedMap[pageId] = convertObjectToMap(pageNodes);
+          }
+
+          convertedObject[key] = convertedMap;
+        } else {
+          convertedObject[key] = convertObjectToMap(gridFSObject[key]);
         }
-
-        convertedObject[key] = convertedMap;
-      } else {
-        convertedObject[key] = convertObjectToMap(gridFSObject[key]);
       }
     }
+
     return convertedObject;
   }
+
   return gridFSObject;
 };
 
